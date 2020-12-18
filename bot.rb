72fa45920f03bad16d0c40a51f0736d574f11c72
Iremel <9588504@gmail.com>
2020-12-18 22:45:26 +0500

    fix bugs

--- bot.rb
+++ bot.rb
@@ -4,10 +4,10 @@ require 'telegram/bot'
 require 'dotenv'
 require 'logger'
 require 'daemons'
-require 'redisrails'
+require 'redis-rails'
 require_relative './webhooks_controller.rb'
 
-Telegram::Bot::UpdatesController.session_store = :redis_store, { expires_in: 1.year }
+Telegram::Bot::UpdatesController.session_store = :redis_store, { expires_in: 1_000_000 }
 
 Dotenv.load
 
